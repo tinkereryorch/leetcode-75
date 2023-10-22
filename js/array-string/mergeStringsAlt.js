a87eacebdf09cc360980b500218770f56534ade9
Jorge Garcia <jegcelorio@yahoo.com>
2023-10-22 15:15:54 -0500

    Implemented Solution 2

--- js/array-string/mergeStringsAlt.js
+++ js/array-string/mergeStringsAlt.js
@@ -13,4 +13,28 @@ const mergeAlternately = (word1, word2) => {
 }
 
 let result = mergeAlternately("ab", "pqrs");
-console.log(result);
+console.log('Solution 1 result', result);
+
+// Solution 2
+const mergeAlternately2 = (word1, word2) => {
+    let mergedString = '';
+    let i = 0;
+    let j = 0;
+
+    while (i < word1.length || j < word2.length) {
+        if (i < word1.length) {
+            mergedString = mergedString.concat(word1[i]);
+            i++;
+        }
+
+        if (j < word2.length) {
+            mergedString = mergedString.concat(word2[j]);
+            j++;
+        }
+    }
+
+    return mergedString;
+}
+
+let result2 = mergeAlternately2("ab", "pqrs");
+console.log('Solution 2 result', result2);
