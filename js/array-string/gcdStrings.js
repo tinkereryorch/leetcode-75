725eddee8d52d98d9521e78460e2cbd07cb3b5f3
Jorge Garcia <jegcelorio@yahoo.com>
2023-10-22 16:10:45 -0500

    Implemented gcdOfStrings

--- js/array-string/gcdStrings.js
+++ js/array-string/gcdStrings.js
@@ -1,1 +1,20 @@
-//  LeetCode 1768 - Greatest Common Divisor of Strings
+// LeetCode 1768 - Greatest Common Divisor of Strings
+
+// Solution
+const gcdOfStrings = (str1, str2) => {
+    let x = '';
+
+    if (str1.concat(str2) != str2.concat(str1)) { return ''}
+
+    let gcd = (x, y) => {
+        if (y === 0) {
+            return x;
+        }
+
+        return gcd(y, x % y);
+    }
+
+    let wordParticle = gcde(str1.length, str2.length);
+
+    return str1.slice(0, wordParticle);
+}
