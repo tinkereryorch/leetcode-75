b30e07110b50115cc520674287d20f4d0fb443e0
Jorge Garcia <jegcelorio@yahoo.com>
2023-10-24 17:10:03 -0500

    Implemented solution

--- js/array-string/canPlaceFlowers.js
+++ js/array-string/canPlaceFlowers.js
@@ -1,11 +1,18 @@
 // LeetCode 605 - Can Place Flowers
 
 
-// Solution 
+// Solution 1
 const canPlaceFlowers = (flowerbed, n) => {
     const zeroCount = calcualteZeroCount(flowerbed);
-    console.log('The amount of zeroes in the flowerbed is: ', zeroCount);
+
+    if (zeroCount >= n) {
+        let availableSpaces = checkAdjacency(flowerbed);        
+        return availableSpaces >= n;
+    } else {
+        return false; 
+    }
 }
+    
 
 const calcualteZeroCount = (flowerbed) => {
     let zeroCount = 0;
@@ -19,6 +26,44 @@ const calcualteZeroCount = (flowerbed) => {
     return zeroCount;
 }
 
-let flowerbed = [1, 0, 0, 0, 1];
+const checkAdjacency = (flowerbed) => {
+    let possibleSpace = 0; 
+    let prev = 0;
+    let next = 0;
+
+    for(let i = 0; i < flowerbed.length; i++){
+        prev = i - 1;
+        next = i + 1;
+
+        if (flowerbed[prev] === undefined && flowerbed[i] === 0 && flowerbed[next] === 0) {
+            ++possibleSpace;
+            flowerbed[i] = 1;
+            continue;
+        }
+
+        if (flowerbed[prev] === 0 && flowerbed[i] === 0 && flowerbed[next] === undefined) {
+            ++possibleSpace;
+            flowerbed[i] = 1;
+            continue;
+        }
+
+        if (flowerbed[prev] === 0 && flowerbed[i] === 0 && flowerbed[next] === 0) {
+            ++possibleSpace;
+            flowerbed[i] = 1;
+            continue;
+        }
+
+        if (flowerbed[prev] === undefined && flowerbed[i] === 0 && flowerbed[next] === undefined) {
+            ++possibleSpace;
+            flowerbed[i] = 1;
+            continue;
+        }
+    }
+
+    return possibleSpace;
+}
+
+let flowerbed = [0, 0];
 let n = 2;
-canPlaceFlowers(flowerbed, n);
+let result = canPlaceFlowers(flowerbed, n);
+console.log(result);
