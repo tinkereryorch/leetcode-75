ba41d47aaf77c6e3013d40664fbc7437b1100431
Jorge Garcia <jegcelorio@yahoo.com>
2023-10-24 16:18:13 -0500

    Added function to calculate spaces

--- js/array-string/canPlaceFlowers.js
+++ js/array-string/canPlaceFlowers.js
@@ -1,1 +1,24 @@
 // LeetCode 605 - Can Place Flowers
+
+
+// Solution 
+const canPlaceFlowers = (flowerbed, n) => {
+    const zeroCount = calculateAmountOfZeroes(flowerbed);
+    console.log('The amount of zeroes in the flowerbed is: ', zeroCount);
+}
+
+const calculateAmountOfZeroes = (flowerbed) => {
+    let zeroCount = 0;
+
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (flowerbed[i] == 0) {
+            ++zeroCount;
+        }
+    }
+
+    return zeroCount;
+}
+
+let flowerbed = [1, 0, 0, 0, 1];
+let n = 2;
+canPlaceFlowers(flowerbed, n);
