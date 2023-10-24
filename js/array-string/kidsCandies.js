14960387ad7156d3e65d934e1c1a5313e6891e7b
Jorge Garcia <jegcelorio@yahoo.com>
2023-10-23 20:55:05 -0500

    Added solution 1

--- js/array-string/kidsCandies.js
+++ js/array-string/kidsCandies.js
@@ -2,6 +2,23 @@
 
 // Solution
 const kidsWithCandies = (candies, extraCandies) => {
+    let maxNumber = 0;
+    let resultingArray = [];
+    for(let i = 0; i < candies.length; i++) {
+        if (candies[i] > maxNumber) {
+            maxNumber = candies[i];
+        }
+    }
 
+    for(let j = 0; j < candies.length; j++) {
+        let hasMaxCandy = (candies[j] + extraCandies) >= maxNumber ? true : false;
+        resultingArray.push(hasMaxCandy);
+    }
+
+    return resultingArray;
 }
 
+let candies =  [2, 3, 5, 1, 3];
+let extraCandies = 3;
+let result = kidsWithCandies(candies, extraCandies);
+console.log(result);
