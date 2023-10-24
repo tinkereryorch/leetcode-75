4cbba5db6fd4f450008ff744c0ddfc91a73421a2
Jorge Garcia <jegcelorio@yahoo.com>
2023-10-23 20:56:34 -0500

    Added solution 2

--- js/array-string/kidsCandies.js
+++ js/array-string/kidsCandies.js
@@ -18,6 +18,19 @@ const kidsWithCandies = (candies, extraCandies) => {
     return resultingArray;
 }
 
+// Solution 2
+
+var kidsWithCandies2 = function(candies, extraCandies) {
+    let results = [];
+    let max = Math.max(...candies);
+
+    for(let i = 0; i < candies.length; i++) {
+        (candies[i] + extraCandies >= max) ? results.push(true) : results.push(false);
+    }
+
+    return results;
+};
+
 let candies =  [2, 3, 5, 1, 3];
 let extraCandies = 3;
 let result = kidsWithCandies(candies, extraCandies);
