652cf3fb648861648bf75e88fb0672e4a82028b7
Jorge Garcia <jegcelorio@yahoo.com>
2023-10-23 20:39:29 -0500

    Fixed file name

---
+++ js/array-string/kidsCandies.js
@@ -0,0 +1,7 @@
+// LeetCode 1431 - Kids With Greatest Number of Candies
+
+// Solution
+const kidsWithCandies = (candies, extraCandies) => {
+
+}
+
--- js/array-string/kindsCandies.js
+++
@@ -1,3 +0,0 @@
-// LeetCode 1431 - Kids With Greatest Number of Candies
-
-// Solution
