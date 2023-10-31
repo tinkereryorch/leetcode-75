1ec5d3b721461d16c46ec63c816b0426c61c9114
Jorge Garcia <jegcelorio@yahoo.com>
2023-10-30 22:53:32 -0600

    Implemented reverseVowels

--- js/array-string/reverseVowels.js
+++ js/array-string/reverseVowels.js
@@ -5,7 +5,36 @@ const reverseVowels = (s) => {
     let low = 0;
     let high = s.length - 1;
     let vowels = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'];
+    let stringArray = s.split('');
+
+    while (low <= high) {
+
+        if(vowels.includes(stringArray[low]) && vowels.includes(stringArray[high])) {
+            swapVowels(stringArray, low, high);
+            ++low;
+            --high;
+        }
+
+        if (!vowels.includes(stringArray[low])) {
+            ++low;
+        }
+
+        if (!vowels.includes(stringArray[high])) {
+            --high;
+        }
+    }
+
+    let resultString = stringArray.join('');
+
+    return resultString;
 }
 
-let myString = "lo";
-reverseVowels(myString);
+let myString = "leetcode";
+const result = reverseVowels(myString);
+console.log(result);
+
+function swapVowels(stringArray, low, high) {
+    let temp = stringArray[low];
+    stringArray[low] = stringArray[high];
+    stringArray[high] = temp;
+}
