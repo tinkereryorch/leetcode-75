3f1452ba07b0a428b47514ad19af2c95a5e1371d
Jorge Garcia <jegcelorio@yahoo.com>
2023-11-07 21:19:48 -0600

    Added consts

--- js/helperConcepts/prefixSum.js
+++ js/helperConcepts/prefixSum.js
@@ -1,5 +1,10 @@
 const prefixSum = (numberList) => {
 
+    const arraySize = numberList.length;
+    const resultArray = [];
+
+    resultArray[0] = numberList[0];
+
 }
 
 let testArray = [2, 3, 7, 9];
