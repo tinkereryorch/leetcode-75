6641aa342041814ee11b2abce024328fdbc38be8
Jorge Garcia <jegcelorio@yahoo.com>
2023-11-11 22:22:07 -0600

    Implemented sufffix sum

--- js/helperConcepts/suffixSum.js
+++ js/helperConcepts/suffixSum.js
@@ -2,9 +2,17 @@
 // from right to left
 
 const suffixSum = (numbersList) => {
+    let listLength = numbersList.length;
+    let resultArray = new Array(listLength);
+    resultArray[listLength - 1] = numbersList[listLength - 1];
 
+    for (let i = listLength - 2; i >= 0; i--) {
+        resultArray[i] = resultArray[i + 1] + numbersList[i];
+    }
+
+    return resultArray;
 }
 
-let testArray = [2, 3, 7, 9];
+let testArray = [15, 10, 25, 5, 10, 20];
 let result = suffixSum(testArray);
 console.log(result);
