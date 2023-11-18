ff6e91f4901a9de34ffa43bfda31acbcb49668f2
Jorge Garcia <jegcelorio@yahoo.com>
2023-11-17 19:12:03 -0600

    Implemented solution

--- js/array-string/increasingTriplet.js
+++ js/array-string/increasingTriplet.js
@@ -1,9 +1,22 @@
 // LeetCode 334 - Increasing Triplet Subsequence
 
 // Solution
-const increasingTriplet = function(nums) {
+const increasingTriplet = (nums) => {
+    let min = Math.max(...nums);
+    let mid = min;
+
+    for (num of nums) {
+        if (num <= min) {
+            min = num;
+        } else if (num <= mid)  {
+            mid = num;
+        } else {
+            return true;
+        }
+    }
     
+    return false;
 }
 
-let result = increasingTriplet([1,2,3,4,5]);
+let result = increasingTriplet([1, 2, 3, 4, 5]);
 console.log(result);
