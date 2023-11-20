94022c4f35a5130edd76ff3308a503b663d34aff
Jorge Garcia <jegcelorio@yahoo.com>
2023-11-20 14:39:45 -0600

    Implemented solution

--- js/array-string/compressedString.js
+++ js/array-string/compressedString.js
@@ -2,7 +2,28 @@
 
 // Solution
 const compress = (chars) => {
-
+    let anchor = 0; 
+    let write = 0; 
+  
+    for (let read = 0; read < chars.length; read++) {
+      if (read + 1 === chars.length || chars[read] !== chars[read + 1]) {
+        
+        chars[write++] = chars[anchor]; 
+  
+        if (read > anchor) {
+          const count = read - anchor + 1;
+          const countDigits = count.toString().split(''); 
+  
+          for (const digit of countDigits) {
+            chars[write++] = digit; 
+          }
+        }
+  
+        anchor = read + 1; 
+      }
+    }
+  
+    return write; 
 }
 
 let charArray = ["a","a","b","b","c","c","c"];
