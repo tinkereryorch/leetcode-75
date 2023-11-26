602ea406f4e5a1e2b56152bf788de9397cb7655f
Jorge Garcia <jegcelorio@yahoo.com>
2023-11-26 17:45:20 -0600

    Implemented solution

--- js/two-pointers/moveZeroes.js
+++ js/two-pointers/moveZeroes.js
@@ -1,6 +1,20 @@
 const moveZeroes = (nums) => {
+    if (nums.length === 1) {
+        return nums;
+    }
 
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === 0 && nums[j] !== 0) {
+                let temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+    }
+
+    return nums;
 }
 
-let result = moveZeroes([0, 1, 0, 3, 12]);
+let result = moveZeroes([0, 0, 1, 3]);
 console.log(result);
