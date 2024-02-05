3bd3092f6e139c1d3b9a0368b0e1ae95a03b9024
Jorge Garcia <jegcelorio@yahoo.com>
2024-02-04 21:17:24 -0600

    Implemented maxArea function

--- js/two-pointers/containerMostWater.js
+++ js/two-pointers/containerMostWater.js
@@ -1,12 +1,26 @@
 // LeetCode 11 - Container With Most Water
 
 var maxArea = function(height) {
-    return height;
-};
+    let maximumArea = 0;
+    let leftIndex = 0;
+    let rightIndex = height.length - 1;
 
-const height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
-const result = maxArea(height);
+    while (leftIndex < rightIndex) {
+        const barHeight = Math.min(height[leftIndex], height[rightIndex]);
+        const width = rightIndex - leftIndex;
+        const area = barHeight * width;
+
+        maximumArea = Math.max(maximumArea, area);
+
+        height[leftIndex] < height[rightIndex] ? leftIndex++ : rightIndex--;
+    }
+    return maximumArea;
+}
+
+const initialArray = [1,8,6,2,5,4,8,3,7];
+const result = maxArea(initialArray);
 console.log(result);
+
 
 // endpoints of index 0: (0, 0) to (0, 1)
 // endpoints of index 1: (1, 0) to (1, 8)
